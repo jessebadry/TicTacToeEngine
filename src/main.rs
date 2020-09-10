4097a4cf5ac4c9455c345f70621422c63cd2b28e
JesseBadry <jessebadry@gmail.com>
2020-09-10 13:49:58 -0700

    init

---
+++ src/main.rs
@@ -0,0 +1,98 @@
+mod ttt_engine;
+use std::io::Read;
+use ttt_engine::{TTTEngine, TTTError, TTTShape};
+fn game_over(engine: &TTTEngine) -> bool {
+    let winner = engine.check_winner();
+    let over = if let Some(winner) = winner {
+        println!("the winner is {}({})", winner.name(), winner.shape());
+        true
+    } else if engine.complete() {
+        println!("it is a tie no winner!");
+        true
+    } else {
+        false
+    };
+    over
+}
+
+fn input(msg: &str) -> std::io::Result<String> {
+    let mut s = String::new();
+    println!("{}", msg);
+    std::io::stdin().read_line(&mut s)?;
+    Ok(s.replace("\r\n", ""))
+}
+
+fn parse_usize(msg: &str, engine: &TTTEngine) -> usize {
+    let board_len = engine.get_board().len();
+    let parse = || {
+        input(msg)
+            .unwrap()
+            .parse::<usize>()
+            .map_err(|_| "Please enter an integer!".to_string())
+            .and_then(|num| {
+                if num >= board_len {
+                    Err("Integer must be from 0 to 2".to_string())
+                } else {
+                    Ok(num)
+                }
+            })
+    };
+
+    let mut num = parse();
+    while num.is_err() {
+        println!("Invalid entry! Reason: {}", num.unwrap_err());
+        println!("{}", engine.board_to_string());
+        num = parse();
+    }
+    num.unwrap()
+}
+fn main() -> Result<(), TTTError> {
+    let mut engine = TTTEngine::new();
+    for i in 0..2 {
+        let name = input(&format!(
+            "Enter name for player{}({})",
+            i + 1,
+            TTTShape::from_usize(i)
+        ))
+        .unwrap_or_else(|why| {
+            println!("Could not get name from stdin! Raw Err: {}", why);
+            std::process::exit(-1);
+        });
+        engine.add_player(&name)?;
+    }
+    let mut rematch = true;
+    while rematch {
+        
+        println!("{}", engine.board_to_string());
+        while !game_over(&engine) {
+            let current_player = engine.get_current_player();
+            println!(
+                "{}({})'s Turn!",
+                current_player.name(),
+                current_player.shape()
+            );
+            let x = parse_usize("Enter x coordinate", &engine);
+            let y = parse_usize("Enter y coordinate", &engine);
+            if let Err(err) = engine.next_turn(x, y) {
+                if let TTTError::IndexOutOfRange = err {
+                    println!("({},{}) is not a proper place on the board!", x, y);
+                } else if let TTTError::ShapeAlreadyPlaced = err {
+                    println!("({}, {}) is already taken!", x, y);
+                }
+            }
+            println!("{}", engine.board_to_string());
+        }
+
+        let mut rematch_choice = String::new();
+        while &rematch_choice != "y" && &rematch_choice != "n" {
+            rematch_choice = input("Rematch? y/n").unwrap();
+            
+        }
+
+        rematch = rematch_choice == "y";
+        if rematch{
+            engine.reset_game();
+        }
+    }
+    Ok(())
+}
---
+++ src/ttt_engine.rs
@@ -0,0 +1,215 @@
+use std::fmt::Debug;
+#[derive(Debug)]
+pub struct Player(TTTShape, String);
+impl Default for Player {
+    fn default() -> Self {
+        Player(TTTShape::Blank, "None".into())
+    }
+}
+impl Player {
+    pub fn name(&self) -> &String {
+        &self.1
+    }
+    pub fn shape(&self) -> &TTTShape {
+        &self.0
+    }
+}
+#[derive(Debug)]
+pub enum TTTError {
+    IndexOutOfRange,
+    ShapeAlreadyPlaced,
+}
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub enum TTTShape {
+    X = 0,
+    O = 1,
+    Blank = 2,
+}
+impl TTTShape {
+    pub fn from_usize(num: usize) -> Self {
+        match num {
+            0 => Self::X,
+            1 => Self::O,
+            _ => Self::Blank,
+        }
+    }
+}
+impl std::fmt::Display for TTTShape {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let name = match self {
+            Self::X => "X",
+            Self::O => "O",
+            Self::Blank => " ",
+        };
+
+        write!(f, "{}", name)
+    }
+}
+impl Default for TTTShape {
+    fn default() -> Self {
+        Self::Blank
+    }
+}
+pub struct TTTEngine {
+    added_player_index: u8,
+    current_player_turn: u8,
+    players: [Player; 2],
+    board: Vec<Vec<TTTShape>>,
+}
+
+impl TTTEngine {
+    pub fn new() -> Self {
+        TTTEngine {
+            current_player_turn: 0,
+            added_player_index: 0,
+            players: Default::default(),
+            board: vec![vec![Default::default(); 3]; 3],
+        }
+    }
+    pub fn get_players(&self) -> &[Player] {
+        &self.players
+    }
+    pub fn reset_game(&mut self) {
+        self.board = vec![vec![Default::default(); 3]; 3];
+        self.current_player_turn = 0;
+    }
+    pub fn get_current_player(&self) -> &Player {
+        &self.players[self.current_player_turn as usize]
+    }
+    pub fn reset_players(&mut self) {
+        self.players = Default::default();
+        self.added_player_index = 0;
+    }
+    pub fn get_board(&self) -> &Vec<Vec<TTTShape>> {
+        &self.board
+    }
+
+    pub fn next_turn(&mut self, x: usize, y: usize) -> Result<(), TTTError> {
+        let board_len = self.board.len();
+        //Before placing shape, check if it is valid.
+        if x >= board_len && y >= self.board[y].len() {
+            return Err(TTTError::IndexOutOfRange);
+        } else if self.board[y][x] != TTTShape::Blank {
+            return Err(TTTError::ShapeAlreadyPlaced);
+        }
+        //Set board at coordinates [x][y] to the shape of the current players turn
+        self.board[y][x] = *self.get_current_player().shape();
+
+        self.current_player_turn = if self.current_player_turn == 1 { 0 } else { 1 };
+        Ok(())
+    }
+    pub fn board_to_string(&self) -> String {
+        let mut output = String::from(" 0|1|2 \n");
+        let divider = "|";
+        for (index, row) in (&self.board).iter().enumerate() {
+            let mut line = String::from("|");
+            for shape in row {
+                line += &format!("{}", shape);
+                line += divider;
+            }
+            line += &format!("{}\n", index);
+            output += &line;
+        }
+        output
+    }
+    pub fn complete(&self) -> bool {
+        self.board
+            .iter()
+            .all(|row| row.iter().all(|shape| *shape != TTTShape::Blank))
+    }
+    fn eq_diagonal<T: PartialEq + Copy>(
+        &self,
+        predicate: impl Fn(T) -> bool,
+        collection: &Vec<Vec<T>>,
+    ) -> bool {
+        let mut i = 0;
+        collection
+            .iter()
+            .all(|collection_val| predicate((collection_val[i], i += 1).0))
+    }
+    fn get_player_from_shape(&self, shape: &TTTShape) -> &Player {
+        &self.players[*shape as usize]
+    }
+    fn check_rows(&self) -> Option<TTTShape> {
+        let mut temp: TTTShape;
+        for row in &self.board {
+            temp = row[0];
+            let row_matches = temp != TTTShape::Blank && row.iter().all(|e| *e == temp);
+            if row_matches {
+                return Some(temp);
+            }
+        }
+        None
+    }
+    fn check_columns(&self) -> Option<TTTShape> {
+        let board_len = self.board.len();
+        //Can use same length as grid is symmetrical
+        for i in 0..board_len {
+            let temp = self.board[0][i];
+            if temp != TTTShape::Blank && (0..board_len).all(|j| self.board[j][i] == temp) {
+                return Some(temp);
+            }
+        }
+
+        None
+    }
+    fn check_diagonal(&self) -> Option<TTTShape> {
+        let temp = self.board[0][0];
+        if temp != TTTShape::Blank && self.eq_diagonal(|shape| shape == temp, &self.board) {
+            Some(temp)
+        } else {
+            None
+        }
+    }
+    fn check_diagonal_rev(&self) -> Option<TTTShape> {
+        let temp = self.board[0][self.board.len() - 1];
+        if temp != TTTShape::Blank && {
+            let mut board = self.board.to_vec();
+            board.reverse();
+            self.eq_diagonal(|shape| shape == temp, &board)
+        } {
+            Some(temp)
+        } else {
+            None
+        }
+    }
+
+    pub fn check_winner(&self) -> Option<&Player> {
+        macro_rules! won {
+            ($func:tt) => {
+                if let Some(winner) = $func() {
+                    return Some(self.get_player_from_shape(&winner));
+                }
+            };
+        }
+        //Check row matching
+        won!((|| self.check_rows()));
+        //Check diagonally
+        won!((|| self.check_diagonal()));
+        won!((|| self.check_diagonal_rev()));
+        //Check columns
+        won!((|| self.check_columns()));
+
+        None
+    }
+    pub fn add_player(&mut self, name: &str) -> Result<(), TTTError> {
+        //if less than 2 we can increment to next enumeration.
+        // else we return and index out of rang error.
+        if self.added_player_index < 2 {
+            let shape = if self.added_player_index == 0 {
+                TTTShape::X
+            } else {
+                TTTShape::O
+            };
+
+            //Set new player to new name
+            self.players[self.added_player_index as usize] = Player(shape, name.into());
+
+            //Increment next index
+            self.added_player_index += 1;
+        } else {
+            return Err(TTTError::IndexOutOfRange);
+        }
+        Ok(())
+    }
+}
